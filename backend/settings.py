436330a94ac18da1037d777fdc3771ae89eafc6e
kingingcole <43896019+kingingcole@users.noreply.github.com>
2019-03-14 00:57:17 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
