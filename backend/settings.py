039072106e8ab60697990f49ea8f1a5137e89586
kingingcole <43896019+kingingcole@users.noreply.github.com>
2019-03-14 00:45:49 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -61,8 +61,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
