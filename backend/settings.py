1509ef908bd30414ea26508f2818fa6e8923bc5b
kingingcole <43896019+kingingcole@users.noreply.github.com>
2019-03-14 02:26:28 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -91,10 +91,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'loaders': [
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader'
-            ],
+            
         },
     },
 ]
