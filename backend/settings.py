d7fa00963ae7ebb91b7d14336262d2b00f579d1f
kingingcole <43896019+kingingcole@users.noreply.github.com>
2019-03-14 01:00:40 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -146,9 +146,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
