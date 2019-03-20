b1578e319d1e6bc6f3d7c9dde3d3d2902c1b677b
kigningcole <43896019+kigningcole@users.noreply.github.com>
2019-03-20 22:44:28 +0200

    removed landmarks field in state

--- states/models.py
+++ states/models.py
@@ -16,13 +16,13 @@ class State(models.Model):
     created_on = models.PositiveIntegerField()
     created_from = models.CharField(max_length=200, null=True, blank=True, default='N/A')
     governor = models.CharField(max_length=500)
-    vice_governor = models.CharField(max_length=500, default='N/A', null=True, blank=True)
+    deputy_governor = models.CharField(max_length=500, default='N/A', null=True, blank=True)
     geopolitical_zone = models.CharField(max_length=100, choices=Zone.choices)
     slogan = models.CharField(max_length=400)
     no_of_local_governments = models.PositiveIntegerField(default='N/A')
     denonyms = models.CharField(max_length=400, null=True, blank=True, default='N/A')
-    landmarks = models.CharField(max_length=1000)
-    website = models.URLField(null=True, blank=True, default='N/A')
+    # landmarks = models.CharField(max_length=1000)
+    website = models.URLField(null=True, blank=True)
 
     def __str__(self):
         return self.name
